d2f076eff02a7a78384dc9de6bb462a6e91e4c4d
paul <paul@xerago.com>
2020-11-06 15:08:28 +0530

    feature/1 - added feature 1 yo
    Took 9 minutes

--- src/main/java/com/serioussamv/tryout/jenkinspipelinedemo/JenkinsPipelineDemoApplication.java
+++ src/main/java/com/serioussamv/tryout/jenkinspipelinedemo/JenkinsPipelineDemoApplication.java
@@ -1,13 +1,22 @@
 package com.serioussamv.tryout.jenkinspipelinedemo;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@Log4j2
 public class JenkinsPipelineDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsPipelineDemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> log.info("feature-1");
 	}
 
 }
